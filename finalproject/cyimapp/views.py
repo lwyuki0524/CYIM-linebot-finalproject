7e920be6bddd440777b1970bc2ff43e56faa6f29
lwyuki0524 <y940068@yahoo.com.tw>
2021-05-04 18:52:13 +0800

    2021/05/04
    #修改views.py檔
    1. 將用戶傳的文字擷取食物標籤(fTag)關鍵字
    (只能擷取一個)
    2. 隨機抽五筆資料

--- finalproject/cyimapp/views.py
+++ finalproject/cyimapp/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
 from cyimapp.models import foodTable
-#from finalproject.settings import BASE_DIR
 from django.conf import settings
 from django.http import HttpResponse,HttpResponseBadRequest,HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextSendMessage,ImageSendMessage,TemplateSendMessage,CarouselTemplate
+from linebot.models import MessageEvent, TextSendMessage,TemplateSendMessage,CarouselTemplate
 
 from urllib import parse#中文URL轉碼
 from templates import replyCarousel
-
+from random import sample
 
 # Create your views here.
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
@@ -30,31 +29,37 @@ def callback(request):
         except LineBotApiError:
             return HttpResponseBadRequest()
         
-        reply_arr=[]#暫存多則訊息(最多五則)
         columns=[]
-        domain = 'https://b393b266aa4b.ngrok.io/' #網域
+        domain = 'https://e0ea1b83be51.ngrok.io'+'/' #網域
         
         for event in events:
             if isinstance(event, MessageEvent):
-                try:
-                    unit = foodTable.objects.filter( fTag = event.message.text ) #讀取一筆資料
-                    if(unit):
-                        print(unit)
-                        for item in unit :
-                            url = domain+parse.quote(str(item.fMenuImage).encode('utf-8'))
-                            print(url)
-                            
-                            message = replyCarousel.CarouselReply(item.fUrl,url,item.fName,item.fAddress )
-                            columns.append(message)
-                            #reply_arr.append( TextSendMessage(text = item.fName+" 網址："+item.fUrl) )
-                            #reply_arr.append( ImageSendMessage(original_content_url=url,preview_image_url=url)  )
-                        carousel_template_message = TemplateSendMessage(
-                            alt_text='Carousel template',template=CarouselTemplate(columns=columns))
-                        line_bot_api.reply_message(event.reply_token, carousel_template_message)
+                
+                #將用戶傳的文字擷取ftag關鍵字
+                filter_text = " "
+                food_entry_list = list(foodTable.objects.all())  #先列出所有食物物件
+                #從物件中一一取出fTag來比對使用者的文字
+                for food_item in food_entry_list:  
+                    if food_item.fTag in event.message.text:
+                        print(event.message.text +" find "+ food_item.fTag)
+                        filter_text = food_item.fTag
                         
-                    else:
-                        line_bot_api.reply_message(event.reply_token,TextSendMessage(text = event.message.text) )
-                except:
+                
+                unit = foodTable.objects.filter( fTag = filter_text ) #過濾資料
+                if unit.exists():
+                    print(unit)
+                    for item in unit :
+                        url = domain+parse.quote(str(item.fMenuImage).encode('utf-8'))
+                        print(url)
+                        message = replyCarousel.CarouselReply(item.fUrl,url,item.fName,item.fAddress )
+                        columns.append(message)
+                    
+                    ##隨機抽五筆資料
+                    carousel_template_message = TemplateSendMessage(alt_text='Carousel template',
+                                                                    template=CarouselTemplate(columns=sample(columns, 5)))
+                    line_bot_api.reply_message(event.reply_token, carousel_template_message)
+                        
+                else:
                     line_bot_api.reply_message(event.reply_token,TextSendMessage(text = event.message.text) )
             
         return HttpResponse()
