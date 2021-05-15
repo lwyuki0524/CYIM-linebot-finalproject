f18b9e4d3a36750c90d4cb234751bba3e4b2e5dc
lwyuki0524 <y940068@yahoo.com.tw>
2021-05-15 17:27:29 +0800

    20210515

--- finalproject/cyimapp/views.py
+++ finalproject/cyimapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from cyimapp.models import foodTable
+#from finalproject.settings import BASE_DIR
 from django.conf import settings
 from django.http import HttpResponse,HttpResponseBadRequest,HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
@@ -10,17 +11,16 @@ from linebot.models import MessageEvent, TextSendMessage,TemplateSendMessage,Car
 
 from urllib import parse#中文URL轉碼
 from templates import replyCarousel
-from random import sample
+
 
 # Create your views here.
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
 
-def listfoodTable(request):
+def listall(request):
     allfoods = foodTable.objects.all().order_by('id')
-    return render(request, "listfoodTable.html", locals())
-
+    return render(request, "listall.html", locals())
 
 
 @csrf_exempt
@@ -36,41 +36,28 @@ def callback(request):
             return HttpResponseBadRequest()
         
         columns=[]
-        domain = 'https://cyim-finalproject.herokuapp.com'+'/' #網域
+        domain = 'https://9944aad41311.ngrok.io/'+'/' #網域
         
         for event in events:
             if isinstance(event, MessageEvent):
                 
-                #讀取資料庫的foodTable
                 if event.message.text=="/read foodTable" :
                     line_bot_api.reply_message(event.reply_token,TextSendMessage(
                         text='https://liff.line.me/'+'1655990146-npeZ9k20') )
                 else:
-                    
-                    #將用戶傳的文字擷取ftag關鍵字
-                    filter_text = " "
-                    food_entry_list = list(foodTable.objects.all())  #先列出所有食物物件
-                    #從物件中一一取出fTag來比對使用者的文字
-                    for food_item in food_entry_list:  
-                        if food_item.fTag in event.message.text:
-                            print(event.message.text +" find "+ food_item.fTag)
-                            filter_text = food_item.fTag
-                            
-                    
-                    unit = foodTable.objects.filter( fTag = filter_text ) #過濾資料
-                    if unit.exists():
+                    unit = foodTable.objects.filter( fTag = event.message.text ) #讀取一筆資料
+                    if(unit):
                         print(unit)
                         for item in unit :
                             url = domain+parse.quote(str(item.fMenuImage).encode('utf-8'))
                             print(url)
+                            
                             message = replyCarousel.CarouselReply(item.fUrl,url,item.fName,item.fAddress )
                             columns.append(message)
-                        
-                        ##隨機抽五筆資料
-                        carousel_template_message = TemplateSendMessage(alt_text='Carousel template',
-                                                                        template=CarouselTemplate(columns=columns))
+                        carousel_template_message = TemplateSendMessage(
+                            alt_text='Carousel template',template=CarouselTemplate(columns=columns))
                         line_bot_api.reply_message(event.reply_token, carousel_template_message)
-                            
+                        
                     else:
                         line_bot_api.reply_message(event.reply_token,TextSendMessage(text = event.message.text) )
             
--- finalproject/finalproject/urls.py
+++ finalproject/finalproject/urls.py
@@ -16,14 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from cyimapp.views import callback
+from cyimapp.views import callback,listall
 from django.conf.urls.static import static
 #from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url('^callback',callback),
-
+    url(r'^listall$', listall),
 ]
 
 #urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
