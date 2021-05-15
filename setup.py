9a69ffb671ccd28f0ae68a647c2f683358c005b8
lwyuki0524 <y940068@yahoo.com.tw>
2021-05-15 20:04:33 +0800

    測試

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='finalproject',
+   version='1.0',
+   description='',
+   author='lwyuki',
+   author_email='',
+   packages=['finalproject'],  #same as name
+   install_requires=[''], #external packages as dependencies
+)
