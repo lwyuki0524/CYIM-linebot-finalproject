974080f14471dba2b553b9a889d8e00f47dee42f
lwyuki0524 <y940068@yahoo.com.tw>
2021-05-17 16:45:59 +0800

    新增快速回覆
    輸入 /飲食區，會跳出快速回覆訊息

--- cyimapp/views.py
+++ cyimapp/views.py
@@ -7,8 +7,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextSendMessage,TemplateSendMessage,CarouselTemplate
-
+from linebot.models import MessageEvent, TextSendMessage,TemplateSendMessage,CarouselTemplate,QuickReply, messages
+from linebot.models import QuickReplyButton,MessageAction,LocationAction
 from urllib import parse#中文URL轉碼
 from templates import replyCarousel
 
@@ -17,13 +17,26 @@ from templates import replyCarousel
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
-#domain = 'https://914d214b2e67.ngrok.io'+'/' #本地端網域       #### 測試時請使用這個(註解下方的domain)####
+#domain = 'https://0681cff91f49.ngrok.io'+'/' #本地端網域       #### 測試時請使用這個(註解下方的domain)####
 domain = 'https://res.cloudinary.com/lwyuki/image/upload/v1'+'/'#### heroku網域(上傳github請使用這個) ####
 
 # show 資料表
 def listfoodTable(request):
     allfoods = foodTable.objects.all().order_by('id')
     return render(request, "listfoodTable.html", locals())
+
+# 快速回覆
+def quick_reply():
+    message=TextSendMessage(
+        text="請選擇功能：",
+        quick_reply=QuickReply(
+        items=[
+            QuickReplyButton(action=MessageAction(label="時段推薦",text="/時段推薦")),#回傳文字
+            QuickReplyButton(action=LocationAction(label="定位搜尋"))#傳回定位資訊
+            ]
+        )
+    )
+    return message
 
 # 隨機選店家
 def randomFood(event):
@@ -69,18 +82,35 @@ def callback(request):
         
         for event in events:
             if isinstance(event, MessageEvent):
-                #如果收到/foodTable，傳資料表
-                if event.message.text=="/foodTable" :
-                    line_bot_api.reply_message(event.reply_token,TextSendMessage(
-                        text='https://liff.line.me/'+'1655990146-npeZ9k20') )
-                #如果收到關鍵字，隨機傳店家資訊
-                elif randomFood(event):
-                    carousel_template_message=randomFood(event)
-                    line_bot_api.reply_message(event.reply_token, carousel_template_message)
-                #鸚鵡回話
-                else:
-                    line_bot_api.reply_message(event.reply_token,TextSendMessage(text = event.message.text) )
+                #文字訊息
+                if event.message.type=='text':
+                    #如果收到/飲食區，傳快速回覆訊息
+                    if event.message.text=="/飲食區" :
+                        msg = quick_reply()
+                        line_bot_api.reply_message(event.reply_token,msg )
+                    #隨機推薦此時段的店家
+                    elif event.message.text=="/時段推薦" :
+                        print("/時段推薦")
+                        line_bot_api.reply_message(event.reply_token,TextSendMessage(text = "還沒做...") )
 
+                    #如果收到/foodTable，傳資料表(測試用)
+                    elif event.message.text=="/foodTable" :
+                        line_bot_api.reply_message(event.reply_token,TextSendMessage(
+                            text='https://liff.line.me/'+'1655990146-npeZ9k20') )
+                    #如果收到分類的關鍵字，隨機傳店家資訊(測試用)
+                    elif randomFood(event):
+                        carousel_template_message=randomFood(event)
+                        line_bot_api.reply_message(event.reply_token, carousel_template_message)
+                    #鸚鵡回話
+                    else:
+                        line_bot_api.reply_message(event.reply_token,TextSendMessage(text = event.message.text) )
+
+                #定位訊息
+                elif event.message.type=='location':
+                    print(event.message)
+                    #傳回經緯度(測試用)
+                    line_bot_api.reply_message(event.reply_token,
+                    TextSendMessage(text = 'latitude：'+ str(event.message.latitude) + '\nlongitude：'+str(event.message.longitude)) )
             
         return HttpResponse()
     else:
