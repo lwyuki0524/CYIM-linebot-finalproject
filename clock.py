b9423d8277ff8ef187c9c86265c0cbb308377451
lwyuki0524 <y940068@yahoo.com.tw>
2021-05-31 02:01:15 +0800

    設定Clock為10分鐘

--- clock.py
+++ clock.py
@@ -10,9 +10,9 @@ sched = BlockingScheduler()
 def timed_job():
     print('This job is run every one minutes.')
 """
-@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/3')
+@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/10')
 def scheduled_job():
-    print('This job runs every day */3 min.')
+    print('This job runs every day */10 min.')
     # 利用datetime查詢時間
     print(f'{datetime.datetime.now().ctime()}')
     url = 'https://cyim-finalproject.herokuapp.com/modifyUbike'
